6db2e9324db55a755bdf2419edc6530136a052e3
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2025-10-08 17:54:27 +0200

    force build error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
+import Navbar from "./Navbar";
 import HomePage from "./pages/HomePage";
 import ProjectListPage from "./pages/ProjectListPage";
 import CreateProjectPage from "./pages/CreateProjectPage";
